33b18f930dabaa9343195fdc1c6362cf9bdb1df8
Ilya Prokhorov <prokhorov_ilya@proton.me>
2026-01-31 16:46:50 +0200

    remove mini.statusline

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -6,16 +6,5 @@ return {
 		require("mini.surround").setup()
 		require("mini.git").setup()
 		require("mini.diff").setup()
-		require("mini.statusline").setup()
-
-		-- Use only HEAD name as summary string
-		local format_summary = function(data)
-			-- Utilize buffer-local table summary
-			local summary = vim.b[data.buf].minigit_summary
-			vim.b[data.buf].minigit_summary_string = summary.head_name or ""
-		end
-
-		local au_opts = { pattern = "MiniGitUpdated", callback = format_summary }
-		vim.api.nvim_create_autocmd("User", au_opts)
-	end,
+  end
 }
