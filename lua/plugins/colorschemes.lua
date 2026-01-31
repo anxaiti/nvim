edcbf1907b04a8c443824e56e7514d321cd8e359
Ilya Prokhorov <prokhorov_ilya@proton.me>
2026-01-31 16:44:50 +0200

    remove unused themes

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,9 +1,7 @@
 return {
 	{ "Mofiqul/dracula.nvim", lazy = true },
-	{ "mellow-theme/mellow.nvim", lazy = true },
 	{ "thesimonho/kanagawa-paper.nvim", lazy = true },
 	{ "catppuccin/nvim", name = "catppuccin", lazy = true },
-	{ "rose-pine/neovim", lazy = true },
 	{
 		"sainnhe/gruvbox-material",
 		lazy = true,
@@ -11,7 +9,4 @@ return {
 			vim.cmd([[let g:gruvbox_material_foreground = 'mix']])
 		end,
 	},
-	{ "folke/tokyonight.nvim", lazy = true },
-	{ "rebelot/kanagawa.nvim", lazy = true },
-	{ "olimorris/onedarkpro.nvim", lazy = true },
 }
